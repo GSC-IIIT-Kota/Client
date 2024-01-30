95e29abb9682d16950dc78c65a18720c4c605c5d
Saatvik Sethi <saatviksethi55@gmail.com>
2024-01-30 23:47:05 +0530

    edu screen text and fonts

--- lib/features/education/screens/resources/resources.dart
+++ lib/features/education/screens/resources/resources.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ResourceScreen extends StatelessWidget {
-  const ResourceScreen({super.key});
+class EducationScreen extends StatelessWidget {
+  const EducationScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text("This is educational resource section. Jo saatvik banane wala hai.")
-          ],
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.pink[200]!, Colors.pink[100]!],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0), // Adjust horizontal padding as needed
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 100.0), // Adjust top padding as needed
+              child: Text(
+                'How is your health today?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 38.0, // Adjust font size to make text wider
+                  fontFamily: "Raleway",
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
