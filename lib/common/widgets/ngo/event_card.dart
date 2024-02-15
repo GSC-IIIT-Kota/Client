fdd9133e95a30d766121374625426403b91f3593
Yogit Nainani <nainaniyogit@gmail.com>
2024-02-15 11:01:27 +0530

    Created Event Profile page

--- lib/common/widgets/ngo/event_card.dart
+++ lib/common/widgets/ngo/event_card.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:solution_challenge/common/widgets/ngo/event_profile.dart';
 import 'package:solution_challenge/common/widgets/texts/video_card_icontext.dart';
 import 'package:solution_challenge/utils/constants/colors.dart';
 import 'package:solution_challenge/utils/constants/sizes.dart';
 import 'package:solution_challenge/utils/helpers/helper_functions.dart';
+import 'package:get/get.dart';
 
 class PEventCard extends StatelessWidget {
   const PEventCard(
       {super.key,
       required this.eventDate,
       required this.eventDayTime,
-      required this.eventTitle, required this.eventLocation, required this.eventDesc});
+      required this.eventTitle,
+      required this.eventLocation,
+      required this.eventDesc,
+      required this.eventPhoto});
 
   final String eventDate;
   final String eventDayTime;
   final String eventTitle;
   final String eventLocation;
   final String eventDesc;
+  final String eventPhoto;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
     return GestureDetector(
-      onTap: () {},
+      onTap: () => Get.to(() => PEventProfile(
+            eventPhoto: eventPhoto,
+            eventTitle: eventTitle,
+            eventDate: eventDate,
+            eventDayTime: eventDayTime,
+            eventLocation: eventLocation,
+            eventDesc: eventDesc,
+          )),
       child: Container(
         width: 300,
         padding: const EdgeInsets.all(1),
@@ -38,79 +51,75 @@ class PEventCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-                const Icon(
-                  Iconsax.clock5,
-                  size: 30,
-                  color: TColors.rani,
-                ),
-
+              const Icon(
+                Iconsax.clock5,
+                size: 30,
+                color: TColors.rani,
+              ),
               Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: TSizes.sm),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          eventDate,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .apply(color: Colors.black, fontWeightDelta: 1),
-                        ),
-                        const SizedBox(
-                          height: TSizes.sm,
-                        ),
-                        Text(
-                          eventDayTime,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .apply(color: TColors.rani),
-                        ),
-                        const SizedBox(
-                          height: TSizes.spaceBtwItems / 2,
-                        ),
-                        Text(
-                          eventTitle,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .apply(color: TColors.battleship, fontWeightDelta: 2),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(
-                          height: TSizes.spaceBtwItems / 2,
-                        ),
-                        PCardIconText(
-                          iconData: Iconsax.location,
-                          iconSize: 20,
-                          iconColor: TColors.dimgrey,
-                          title: eventLocation,
-                          titleStyle: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .apply(color: TColors.battleship),
-                        ),
-                        const SizedBox(
-                          height: TSizes.spaceBtwItems / 2,
-                        ),
-                        Text(
-                          eventDesc,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .apply(color: TColors.battleship),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 3,
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
-                    ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: TSizes.sm),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        eventDate,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodySmall!
+                            .apply(color: Colors.black, fontWeightDelta: 1),
+                      ),
+                      const SizedBox(
+                        height: TSizes.sm,
+                      ),
+                      Text(
+                        eventDayTime,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodySmall!
+                            .apply(color: TColors.rani),
+                      ),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems / 2,
+                      ),
+                      Text(
+                        eventTitle,
+                        style: Theme.of(context).textTheme.bodySmall!.apply(
+                            color: TColors.battleship, fontWeightDelta: 2),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        textAlign: TextAlign.left,
+                      ),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems / 2,
+                      ),
+                      PCardIconText(
+                        iconData: Iconsax.location,
+                        iconSize: 20,
+                        iconColor: TColors.dimgrey,
+                        title: eventLocation,
+                        titleStyle: Theme.of(context)
+                            .textTheme
+                            .bodySmall!
+                            .apply(color: TColors.battleship),
+                      ),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems / 2,
+                      ),
+                      Text(
+                        eventDesc,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .apply(color: TColors.battleship),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 3,
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
                   ),
-
+                ),
               )
             ],
           ),
