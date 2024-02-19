d128dad7e911c0d9eace7642ca4cbae10ff65ea8
Yogit Nainani <nainaniyogit@gmail.com>
2024-02-20 00:19:47 +0530

    Fix Onboarding screens ui and controller

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -9,9 +9,9 @@ class TImages {
   static const String facebook = "assets/logos/facebook.png";
 
   // Onboarding images
-  static const String onBoardingImage1 = "assets/images/onboarding_images/screen1.png";
-  static const String onBoardingImage2 = "assets/images/onboarding_images/screen2.png";
-  static const String onBoardingImage3 = "assets/images/onboarding_images/screen3.png";
+  static const String onBoardingImage1 = "assets/images/onboarding_images/screen_1.png";
+  static const String onBoardingImage2 = "assets/images/onboarding_images/screen_2.png";
+  static const String onBoardingImage3 = "assets/images/onboarding_images/screen_3.png";
 
   // Login_signup images
   static const String verifyEmailImage = "assets/images/login_signup/verifyemail.png";
