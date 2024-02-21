192809c83636377134eb88f8b57fdcecbd01d54d
Saatvik Sethi <saatviksethi55@gmail.com>
2024-02-21 22:12:46 +0530

    Integrated ViewAll

--- lib/common/widgets/viewall/viewall_cards.dart
+++ lib/common/widgets/viewall/viewall_cards.dart
@@ -6,12 +6,17 @@ import 'package:solution_challenge/common/widgets/education/videos/video_card.da
 import 'package:solution_challenge/common/widgets/ngo/campaign_card.dart';
 import 'package:solution_challenge/common/widgets/ngo/event_card.dart';
 import 'package:solution_challenge/common/widgets/ngo/organization_card.dart';
-import 'package:solution_challenge/utils/constants/image_strings.dart';
+import 'package:solution_challenge/models/campaign.dart';
+import 'package:solution_challenge/models/event.dart';
+import 'package:solution_challenge/models/organisation.dart';
+import 'package:solution_challenge/services/campaign_service.dart';
+import 'package:solution_challenge/services/education/video_service.dart';
+import 'package:solution_challenge/services/event_service.dart';
+import 'package:solution_challenge/services/ngo_service.dart';
 import 'package:solution_challenge/utils/constants/sizes.dart';
 import 'package:solution_challenge/utils/helpers/helper_functions.dart';
 
 import '../../../models/education/video.dart';
-import '../education/articles/home_article_card.dart';
 
 class PViewAllScreen extends StatelessWidget {
   const PViewAllScreen({super.key, required this.initiativeType});
@@ -30,8 +35,6 @@ class PViewAllScreen extends StatelessWidget {
                   const SizedBox(
                     height: TSizes.spaceBtwItems,
                   ),
-
-                  /// Appbar
                   PAppBar(
                     showBackArrow: true,
                     title: Text(
@@ -45,7 +48,6 @@ class PViewAllScreen extends StatelessWidget {
                   const SizedBox(
                     height: TSizes.spaceBtwItems,
                   ),
-
                   PSearchContainer(
                     text: 'Search $initiativeType',
                   ),
@@ -55,199 +57,140 @@ class PViewAllScreen extends StatelessWidget {
                 ],
               ),
             ),
-
-            ///Body
             if (initiativeType == 'Campaigns')
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    PCampaignCard(
-                      title: "Help these kids get money to study",
-                      description:
-                          "This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids.",
-                      raisedMoney: 2000,
-                      totalGoal: 4000,
-                      imageUrl: TImages.banner1Image,
-                      orgPhoto:
-                          'https://pbs.twimg.com/profile_images/1601849162730905601/IskNG8bF_400x400.jpg',
-                      cardWidth: PHelperFunctions.screenWidth(),
-                      rightMargin: EdgeInsets.zero,
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                    PCampaignCard(
-                      title: "Help these kids get money to study",
-                      description:
-                          "This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids. This org has description. It works for female children to get paid for their work. And the org is working really hard to get money for these kids.",
-                      raisedMoney: 2000,
-                      totalGoal: 4000,
-                      imageUrl: TImages.banner1Image,
-                      orgPhoto:
-                          'https://pbs.twimg.com/profile_images/1601849162730905601/IskNG8bF_400x400.jpg',
-                      cardWidth: PHelperFunctions.screenWidth(),
-                      rightMargin: EdgeInsets.zero,
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                  ],
-                ),
+              FutureBuilder<List<Campaign>>(
+                future: CampaignService.getAllCampaigns(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    return Column(
+                      children: snapshot.data!.map((campaign) {
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: TSizes.defaultSpace),
+                              child: PCampaignCard(
+                                title: campaign.title,
+                                description: campaign.description,
+                                raisedMoney: campaign.raisedMoney,
+                                totalGoal: campaign.totalGoal,
+                                imageUrl: campaign.imageUrl,
+                                orgPhoto: campaign.imageUrl,
+                                cardWidth: PHelperFunctions.screenWidth(),
+                                rightMargin: EdgeInsets.zero,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            // Add space between items
+                          ],
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
               ),
-
             if (initiativeType == 'Videos')
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    PVideoCard(
-                      video: Video(
-                          id: '2',
-                          title: 'Video 2',
-                          uploader: 'Uploader 2',
-                          uploadDate: DateTime.now(),
-                          description: 'Description 2',
-                          tags: ['Tag 3', 'Tag 4'],
-                          category: 'Category 2',
-                          thumbnailUrl: 'Thumbnail 2',
-                          comments: [],
-                          likes: 20,
-                          transcripts: 'Transcripts 2'),
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                    PVideoCard(
-                        video: Video(
-                            id: '2',
-                            title: 'Video 2',
-                            uploader: 'Uploader 2',
-                            uploadDate: DateTime.now(),
-                            description: 'Description 2',
-                            tags: ['Tag 3', 'Tag 4'],
-                            category: 'Category 2',
-                            thumbnailUrl: 'Thumbnail 2',
-                            comments: [],
-                            likes: 20,
-                            transcripts: 'Transcripts 2')),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                  ],
-                ),
+              FutureBuilder<List<Video>>(
+                future: VideoService.getVideos(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    return Column(
+                      children: snapshot.data!.map((video) {
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: TSizes.defaultSpace),
+                              child: PVideoCard(video: video),
+                            ),
+                            const SizedBox(height: 16),
+                            // Add space between items
+                          ],
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
               ),
-
-            if (initiativeType == 'Articles')
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    PHomeArticleCard(
-                      articleImg: TImages.banner4Image,
-                      articleCategory: 'Menstrual Hygiene',
-                      readingTime: '7 min',
-                      uploadTime: '2d ago',
-                      articleTitle:
-                          'Downside of using reusable pads during menstrual cycles',
-                      hasAuthor: false,
-                      articleAuthor: 'Admin',
-                      articleContent:
-                          'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
-                    ),
-                    PHomeArticleCard(
-                      articleImg: TImages.banner4Image,
-                      articleCategory: 'Menstrual Hygiene',
-                      readingTime: '7 min',
-                      uploadTime: '2d ago',
-                      articleTitle:
-                          'Downside of using reusable pads during menstrual cycles',
-                      hasAuthor: false,
-                      articleAuthor: 'Admin',
-                      articleContent:
-                          'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
-                    ),
-                    SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                  ],
-                ),
-              ),
-
             if (initiativeType == 'Events')
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    PEventCard(
-                      eventDate: '26th February, 2024',
-                      eventTitle: 'Buy me pad, donation event annual for women',
-                      eventLocation: 'St. Petersberg College',
-                      eventDesc:
-                          'Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
-                      eventPhoto: TImages.banner2Image,
-                      cardWidth: PHelperFunctions.screenWidth(),
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                    PEventCard(
-                      eventDate: '26th February, 2024',
-                      eventTitle: 'Buy me pad, donation event annual for women',
-                      eventLocation: 'St. Petersberg College',
-                      eventDesc:
-                          'Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
-                      eventPhoto: TImages.banner2Image,
-                      cardWidth: PHelperFunctions.screenWidth(),
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                  ],
-                ),
+              FutureBuilder<List<Event>>(
+                future: EventService().getEvents(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    return Column(
+                      children: snapshot.data!.map((event) {
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: TSizes.defaultSpace),
+                              child: PEventCard(
+                                eventDate: "${event.uploadDate}",
+                                eventTitle: event.title,
+                                eventLocation: event.location,
+                                eventDesc: event.description,
+                                eventPhoto: event.banner,
+                                cardWidth: PHelperFunctions.screenWidth(),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            // Add space between items
+                          ],
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
               ),
-
             if (initiativeType == 'Organizations')
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    POrganizationCard(
-                      cardWidth: PHelperFunctions.screenWidth(),
-                      orgPhoto:
-                          'https://pbs.twimg.com/profile_images/1601849162730905601/IskNG8bF_400x400.jpg',
-                      ngoName: 'NGO for Women',
-                      ngoLocation: 'Rajasthan, India',
-                      id: '',
-                      email: '',
-                      passwordHash: '',
-                      campaigns: const [],
-                      events: const [],
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                    POrganizationCard(
-                      cardWidth: PHelperFunctions.screenWidth(),
-                      orgPhoto:
-                          'https://pbs.twimg.com/profile_images/1601849162730905601/IskNG8bF_400x400.jpg',
-                      ngoName: 'NGO for Women',
-                      ngoLocation: 'Rajasthan, India',
-                      id: '',
-                      email: '',
-                      passwordHash: '',
-                      campaigns: const [],
-                      events: const [],
-                    ),
-                    const SizedBox(
-                      height: TSizes.spaceBtwSections,
-                    ),
-                  ],
-                ),
+              FutureBuilder<List<NGO>>(
+                future: NGOService().getAllNGOs(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    return Column(
+                      children: snapshot.data!.map((organization) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: TSizes.defaultSpace),
+                          child: Column(
+                            children: [
+                              POrganizationCard(
+                                cardWidth: PHelperFunctions.screenWidth(),
+                                orgPhoto: organization.profile?.logo ?? '',
+                                ngoName: organization.profile?.ngoName ?? '',
+                                ngoLocation:
+                                    organization.profile?.address ?? '',
+                                id: organization.id ?? '',
+                                email: organization.email ?? '',
+                                passwordHash: organization.passwordHash ?? '',
+                                campaigns: organization.campaigns ?? [],
+                                events: organization.events ?? [],
+                              ),
+                              const SizedBox(height: TSizes.spaceBtwSections),
+                              // Add space between cards
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  }
+                },
               ),
           ],
         ),
